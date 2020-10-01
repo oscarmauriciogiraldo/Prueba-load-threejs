ec261a4d007c5256df33a7fe47aed69f865e313f
Oscar Giraldo <oscardan@utp.edu.co>
2020-10-01 17:56:04 -0500

    Hola Mundo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 var scene, camera, renderer, mesh;
 
+function init(){
+    // Crea el objeto escena, utilizando la libreria three
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(90, 1283/720, 0.7, 1000);
+
+    //crea el mesh
+    mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1,1,1),
+        new THREE.MeshBasicMaterial({color:0xff9999, wireframe:true})
+        //wireframe muestra el cubo solido si esta en true
+    );
+    //agrega la malla a la scena
+    scene.add(mesh);
+
+    //creando el renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(1280, 720);
+    document.body.appendChild(renderer.domElement);
+
+    //s ellama la funcion animate
+    animate();
+}
+
+function animate(){
+
+    requestAnimationFrame(animate);
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
+}
+
 window.onload = init;
